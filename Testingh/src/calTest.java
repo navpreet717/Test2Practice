69bc99f59d16fed27bade2ef66782d5e1811320c
navpr <navpr@192.168.0.13>
2019-08-18 22:44:37 -0400

    simple testing

---
+++ Testingh/src/calTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class calTest {
+
+	@Test
+	public void caltest1() {
+		cal c2=new cal();
+		assertEquals(13,c2.add(6, 7));
+	}
+
+}
